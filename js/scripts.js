3080fdd3ea48235f1c63ebad65054733b6e8a46d
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-08 10:43:12 -0700

    send array to a ul using append.
    co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,16 @@ $(document).ready(function() {
     const book = $("#book").val();
     const band = $("#band").val();
     const color = $("#color").val();
+
     let favoriteArray1 = [movie, food, book, band, color];
     let favoriteArray2 = [];
     favoriteArray2.push(favoriteArray1[1], favoriteArray1[0], favoriteArray1[2]);
-    console.log(favoriteArray2);
+
+    $("#li1").append(favoriteArray1[0]);
+    $("#li2").append(favoriteArray1[1]);
+    $("#li3").append(favoriteArray1[2]);
+    $("#li4").append(favoriteArray1[3]);
+    $("#li5").append(favoriteArray1[4]);
 
   });
 });  
